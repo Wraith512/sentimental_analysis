3944e43185504c620c2610f755606063da13ac2c
Samar Rai <rsamar918@gmail.com>
2026-03-10 00:38:25 +0530

    Refactor sentimental analysis code structure

--- pkl.py
+++ pkl.py
@@ -1,13 +1,6 @@
-# %% [markdown]
 # SENTIMENTAL ANALYSIS (using tweepy)
-# 
 
-# %%
-#this is the code for sentimental analysis for twitter 
-#using twitter api for realtime analysis (tweepy)
 
-# %%
-#importing the necesssary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,11 +12,6 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 import joblib
 import re
 
-##basic eda
-
-
-# %%
-#importing the dataset 
 df = pd.read_csv(
     r"C:\Users\ASUS\Desktop\archive\training.1600000.processed.noemoticon.csv",
     header=None
@@ -31,21 +19,19 @@ df = pd.read_csv(
 
 df.columns = ["sentiment", "id", "date", "query", "user", "text"]
 
-# %%
+
 df.head()
 
-# %%
+
 df.shape()
 
-# %%
+
 df = df[["sentiment", "text"]]
 
 df.head()
 
-#converting the sentiment nummbers to labels from 0-negative and 4-postive 
 df["sentiment"] = df["sentiment"].replace({0: "negative", 4: "positive"})
 
-#checking missing and duplicate values
 print(df.isnull().sum())
 print("Duplicates:", df.duplicated().sum())
 
@@ -65,40 +51,37 @@ df["clean_text"] = df["text"].apply(clean_text)
 
 df.head()
 
-#defining the features and the labels 
 X = df["clean_text"]
 y = df["sentiment"]
 
-#splitting the data into train and test 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#tf-idf vectorizer to convert the words to numbers without loosing the semantic valie 
 vectorizer = TfidfVectorizer(max_features=5000)
 
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
-#training the model 
+
 model = LogisticRegression(max_iter=200)
 
 model.fit(X_train_tfidf, y_train)
 
-#printing the model accuracy
 y_pred = model.predict(X_test_tfidf)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:\n")
 print(classification_report(y_test, y_pred))
 
-#saving the model
+
 joblib.dump(model, "sentiment_analysis_model.pkl")
 joblib.dump(vectorizer, "tfidf_vectorizer.pkl")
 
-#loading the model for further use 
+
 model = joblib.load("sentiment_analysis_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
 
 
 
