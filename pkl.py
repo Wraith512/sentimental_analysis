f24560c652a8fedd4eda0c7aeb6a9cf192c515bb
Samar Rai <rsamar918@gmail.com>
2026-03-17 20:33:16 +0530

    Replace Logistic Regression with SVM model

--- pkl.py
+++ pkl.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from sklearn.model_selection  import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
